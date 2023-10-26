f226b65cc1749bf220d6694abd1954172aebff62
Sterling Meggs <sterlynx@gmail.com>
2023-10-26 16:41:51 -0700

    Hotfix1

--- app/routes.py
+++ app/routes.py
@@ -10,59 +10,9 @@ from app.models import User
 def index():
     return render_template('index.html')
 
-@app.route('/links')
-def links():
-    return render_template('links.html')
-
-# Create a second route
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    # Create an instance of the SignUpForm
-    form = SignUpForm()
-    if form.validate_on_submit():
-        # Get the data from each of the fields
-        first_name = form.first_name.data
-        last_name = form.last_name.data
-        username = form.username.data
-        email = form.email.data
-        password = form.password.data
-        # print(first_name, last_name, username, email, password)
-
-        # Check to see if we already have a User with that username or email
-        check_user = db.session.execute(db.select(User).where( (User.username==username) | (User.email==email) )).scalars().all()
-        if check_user:
-            flash('A user with that username and/or email already exists')
-            return redirect(url_for('signup'))
-        # Create a new instance of the User class with the data from the form
-        new_user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
-        # Add the new user object to the database
-        db.session.add(new_user)
-        db.session.commit()
-
-        flash(f"{new_user.username} has been created!")
-
-        # Redirect back to the home page
-        return redirect(url_for('index'))
-    return render_template('signup.html', form=form)
+@app.route('/topfive')
+def topfive():
+    top_five_img = []
+    return render_template('topfive.html')
 
 
-@app.route('/login', methods=["GET","POST"])
-def login():
-    # Create an instance of the LoginForm
-    form = LoginForm()
-    if form.validate_on_submit():
-        # Get the data from the form
-        username = form.username.data
-        password = form.password.data
-        remember_me = form.remember_me.data
-        # Query the User table for a user with that username
-        user = db.session.execute(db.select(User).where(User.username==username)).scalar()
-        # Check if there is a user AND the password is correct for that user
-        if user is not None and user.check_password(password):
-            # log the user in via Flask-Login
-            flash(f'{user.username} has succesfully logged in.')
-            return redirect(url_for('index'))
-        else:
-            flash('Incorrect username and/or password')
-            return redirect(url_for('login'))
-    return render_template('login.html', form=form)
